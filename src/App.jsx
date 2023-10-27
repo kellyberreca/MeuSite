4af1152e4c447fb524d94e5b64e043a0a4ff41af
cremosinho <fabricio-fjc@outlook.com>
2023-10-27 11:59:43 -0300

    estilização de container

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import './App.css'
 import Content from './components/Content'
+import Projetos from './components/Projetos'
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <Content>
-        <div>
-          Hello world!
-        </div>
+        <Projetos/>
       </Content>
     </>
   )
---
+++ src/components/Projetos.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../App.css"
+import Card from "./partes/Card";
+
+export default props =>{
+    return(
+        <>
+            <Card 
+            imgSource="https://images.unsplash.com/photo-1695669882447-1de80022ff21?auto=format&fit=crop&q=80&w=2071&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+            imgAlt="" label="meu projeto"/>
+        </>
+    )
+}
